4993cc4ffc0f5f7a6bd87db78005ebda6639b0a8
Margarita <m.novolokova@gmail.com>
2023-01-04 16:20:24 +0200

    flex-direction on JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,16 @@
 // За допомогою дата-атрибутів реалізувати інтерактивну шпаргалку для візуалізації якості flex-direction.
 // Приклад на малюнку.
 
+const [wrap] = document.getElementsByClassName("wrap-div");
+const btns = document.querySelectorAll(".btn button");
+for (const btn of btns) {
+  btn.addEventListener("click", ({ target: { dataset } }) => {
+    btns.forEach((element) => {
+      element.style.backgroundColor = "transparent";
+      element.style.color = "black";
+    });
+    btn.style.backgroundColor = "#4fc2f8";
+    btn.style.color = "white";
+    wrap.style.flexDirection = dataset.flex;
+  });
+}
