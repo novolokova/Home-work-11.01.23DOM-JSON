bd0da2904ca4e28f85ac5f3e35c2413ce9b7a328
Margarita <m.novolokova@gmail.com>
2023-01-05 23:19:29 +0200

    Refactoring code

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,16 +3,16 @@
 // За допомогою дата-атрибутів реалізувати інтерактивну шпаргалку для візуалізації якості flex-direction.
 // Приклад на малюнку.
 
-const [wrap] = document.getElementsByClassName("wrap-div");
 const btns = document.querySelectorAll(".btn button");
 for (const btn of btns) {
-  btn.addEventListener("click", ({ target: { dataset } }) => {
+  btn.addEventListener("click", ({target}) => {
     for (const element of btns) {
       element.style.backgroundColor = "transparent";
       element.style.color = "black";
     }
     btn.style.backgroundColor = "#4fc2f8";
     btn.style.color = "white";
-    wrap.style.flexDirection = dataset.flex;
+    target.parentElement.nextElementSibling.style.display = 'flex';
+    target.parentElement.nextElementSibling.style.flexDirection = target.dataset.flex;
   });
 }
