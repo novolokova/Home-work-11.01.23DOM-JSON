fbe7353a95ed71e66faa7c040184489e9adbf48d
Margarita <m.novolokova@gmail.com>
2023-01-11 15:04:33 +0200

    Initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,13 @@
 "use strict";
 
-// За допомогою дата-атрибутів реалізувати інтерактивну шпаргалку для візуалізації якості flex-direction.
-// Приклад на малюнку.
 
-const btns = document.querySelectorAll(".btn button");
-for (const btn of btns) {
-  btn.addEventListener("click", ({target}) => {
-    for (const element of btns) {
-      element.style.backgroundColor = "transparent";
-      element.style.color = "black";
-    }
-    btn.style.backgroundColor = "#4fc2f8";
-    btn.style.color = "white";
-    target.parentElement.nextElementSibling.style.display = 'flex';
-    target.parentElement.nextElementSibling.style.flexDirection = target.dataset.flex;
-  });
-}
+
+
+
+
+
+
+
+
+
+
